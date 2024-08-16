a8bb72f630cf09debfd9d8757774bd4f5a8ad1bb
TDS <tds@tcg3pl.com>
2024-08-16 09:26:59 -0400

    Adding title and header.

--- barcode_generator.py
+++ barcode_generator.py
@@ -4,3 +4,24 @@ import pandas as pd
 st.title('TCG BarCode Generator')
 st.divider()
 st.header('Select BarCode type and enter value')
+
+bc_types = ['code128',
+             'code39',
+             'ean',
+             'ean13',
+             'ean14',
+             'ean8',
+             'gs1_128',
+             'gtin',
+             'isbn10',
+             'issn',
+             'itf',
+             'pzn',
+             'upc',
+             'upca']
+
+col1, col2 = st.columns(2)
+with col1:
+    bc_type = st.selectbox('BarCode Type', (bc_types))
+with col2:
+    bc_value = st.text_input('BarCode Value')
