ee997c2386abcf06b5b138f72326f1429c21d265
TDS <tds@tcg3pl.com>
2024-08-16 13:27:46 -0400

    Streamlining after testing on local host.

--- barcode_generator.py
+++ barcode_generator.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import barcode
-
+from barcode.writer import ImageWriter
+from PIL import Image
+import cairosvg
 
 st.title('TCG BarCode Generator')
 st.divider()
@@ -25,54 +27,24 @@ col1, col2 = st.columns(2)
 with col1:
     bc_type = st.selectbox('BarCode Type', (bc_types))
 with col2:
-    bc_value = st.text_input('BarCode Value')
+    bc_value = st.text_input('BarCode Value', value='123456789012')
 
-# try:
-#     EAN = barcode.get_barcode_class(bc_type)
-#     my_ean = EAN(bc_value)
-#     fullname = my_ean.save(f'{bc_type}_barcode')
-#     st.write(fullname)
-# except Exception as e:
-#     st.write(f'Skipping {bc_type} because {e}')
+try:
+    EAN = barcode.get_barcode_class(bc_type)
+    my_ean = EAN(bc_value, writer=ImageWriter())
+    fullname = my_ean.save(f'{bc_type}_barcode_{bc_value}')
+    st.write(fullname)   
+except Exception as e:
+    st.write(f'Skipping {bc_type} because {e}')
 
-# # import EAN13 from barcode module 
-# from barcode import EAN13 
+st.image(fullname, caption="Generated Barcode")
 
-# # import ImageWriter to generate an image file 
-# from barcode.writer import ImageWriter 
-
-# # Make sure to pass the number as string 
-# number = '5901234123457'
-
-# # Now, let's create an object of EAN13 class and 
-# # pass the number with the ImageWriter() as the 
-# # writer 
-# my_code = EAN13(number, writer=ImageWriter()) 
-
-# # Our barcode is ready. Let's save it. 
-# my_code.save("new_code1")
-
-# Function to generate barcode
-def generate_barcode(number):
-    barcode = EAN13(number, writer=ImageWriter())
-    barcode.save("barcode")
-    return "barcode.png"
-
-# Streamlit app
-st.title("Barcode Generator")
-
-# Input for barcode number
-number = st.text_input("Enter a 12-digit number for the barcode:")
-
-if st.button("Generate Barcode"):
-    if len(number) == 12 and number.isdigit():
-        barcode_image = generate_barcode(number)
-        st.image(barcode_image, caption="Generated Barcode")
-    else:
-        st.error("Please enter a valid 12-digit number.")
-
-
-st.image(my_code)
-st.image(fullname)
+with open(f'{fullname}', "rb") as file:
+    btn_png = st.download_button(
+        label="Download PNG",
+        data=file,
+        file_name=f"{fullname[:-4]}.png",
+        mime="image/png",
+    )
 
 
